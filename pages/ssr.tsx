f175e4989bd8a1ec7af8e68d76c89969d3b5d729
yto-tkg <yuto-takagi@cybozu.co.jp>
2022-09-01 08:03:14 +0900

    ssg, ssr, isr pructice

---
+++ pages/ssr.tsx
@@ -0,0 +1,35 @@
+import { GetServerSideProps, NextPage } from 'next'
+import Head from 'next/head'
+
+type SSRProps = {
+  message: string
+}
+
+const SSR: NextPage<SSRProps> = (props) => {
+  const { message } = props
+
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href='/favicon.ico' />
+      </Head>
+      <main>
+        <p>{message}</p>
+      </main>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<SSRProps> = async (context) => {
+  const timestamp = new Date().toLocaleString()
+  const message = `${timestamp}にこのページのgetServerSidePropsが実行されました`
+
+  return {
+    props: {
+      message
+    }
+  }
+}
+
+export default SSR
